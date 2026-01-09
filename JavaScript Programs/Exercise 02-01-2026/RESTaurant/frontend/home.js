ce35fedeee97bc76bd55a7e279005626de361481
AniketW42 <aniketwakte42@gmail.com>
2026-01-09 12:28:12 +0530

    assingment updated

--- JavaScript Programs/Exercise 02-01-2026/RESTaurant/frontend/home.js
+++ JavaScript Programs/Exercise 02-01-2026/RESTaurant/frontend/home.js
@@ -1,9 +1,5 @@
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
   const time = document.getElementById("time");
-  const time2 = document.getElementById("http-time");
   async function get_time() {
     try {
       const response = await fetch("http://localhost:3000/get-time");
@@ -14,17 +10,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
-  async function get_time_http() {
-    try {
-      const response = await fetch("http://localhost:4000/get-time");
-      const data = await response.json(); 
-      time2.textContent = data.time.toUpperCase();
-
-    } catch (err) {
-      console.error("Failed to fetch time:", err);
-    }
-  }
-
   setInterval(get_time, 1000)
-  setInterval(get_time_http, 1000)
+  
 });
